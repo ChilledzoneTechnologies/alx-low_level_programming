0db596f2ca459c1dbc2cb1e31f9bc4da1adfcb34
ChilledzoneTechnologies <narteytettehe@gmail.com>
2023-08-15 20:31:42 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@
  */
 
 int main(void)
-
+{
 	char _putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	unsigned int c;
 	for (c = 0; c < sizeof(_putchar); c++)
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
+
