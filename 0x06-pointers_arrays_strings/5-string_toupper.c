4eead054dc22e7acd39c5f8ffce39c81f04b2eb7
ChilledzoneTechnologies <narteytettehe@gmail.com>
2023-08-24 14:56:56 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,11 +12,11 @@ char *string_toupper(char *)
 
 	while (s[i] != '\0')
 	{
-		if ((s[i] >= 97) && (s[i] <= 122))
+		if ([i] >= 97) && ([i] <= 122))
 		{
-			s[i] = s[i] - 32;
+			[i] = [i] - 32;
 		}
 	i++;
 	}
-	return (s);
+	return (0);
 }
