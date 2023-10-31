0d4a5a577cda8f3fb91411bb4f13676fd9ddb1f6
ChilledzoneTechnologies <narteytettehe@gmail.com>
2023-10-30 21:30:37 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * append_text_to_file - function appends text at the end of a file
+ * append_text_to_file -function appends text at the end of a file
  * @filename: filename.
  * @text_content: content.
  *
